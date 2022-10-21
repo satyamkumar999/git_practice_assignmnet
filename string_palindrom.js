3ae4c3b49c31e9d26fda2a11f3507f86543b19f7
satyam <mintoo9927@gmail.com>
2022-10-21 22:11:30 +0530

    Check whether a string is palindrome or not

---
+++ string_palindrom.js
@@ -0,0 +1,15 @@
+let newest="";
+
+    for(let i=N-1; i>=0; i--){
+
+        newest+=str[i];
+    }
+
+    if(newest==str){
+
+        console.log("Yes");
+
+    }else{
+
+        console.log("No");
+    }
